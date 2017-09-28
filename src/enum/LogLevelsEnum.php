b9d50b78e4cdfd00e69faad68842952e9db00713
GOMEL\Mikalai_Kabzar <loki.engine@mail.ru>
2017-09-28 15:27:39 +0300

    Add log levels file.

---
+++ src/enum/LogLevelsEnum.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ReportPortalBasic\Enum;
+
+/**
+ * Enum describes log levels.
+ *
+ * @author Mikalai_Kabzar
+ */
+class LogLevelsEnum
+{
+    const ERROR = 'error';
+
+    const WARN = 'warn';
+
+    const INFO = 'info';
+
+    const DEBUG = 'debug';
+
+    const TRACE = 'trace';
+
+    const FATAL = 'fatal';
+
+    const UNKNOWN = 'unknown';
+
+}
